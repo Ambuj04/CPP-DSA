746ccabf4b5b5625492de936339a4d48d435bbb5
ambuj <ambujsdl2004@gmail.com>
2025-08-02 19:47:35 +0530

    imp stack ques added

--- 17. stack/question/nextsmallerElem.cpp
+++ 17. stack/question/nextsmallerElem.cpp
@@ -2,15 +2,9 @@
 #include<vector>
 #include<stack>
 using namespace std;
-
-int main(){
- vector<int>v;
- v.push_back(2);
- v.push_back(3);
- v.push_back(5);
- v.push_back(1); 
- v.push_back(4); 
- stack<int>s;
+vector<int> nextSmaller(vector<int>&v){
+   //right to left
+stack<int>s;
  s.push(-1);
  vector<int>ans(v.size());
  for(int i = v.size()-1;i>=0;i--){
@@ -21,9 +15,43 @@ int main(){
     ans[i] = s.top();
     s.push(curr);
  }
+ return ans;
+}
+
+void print(vector<int>&ans){
  
  for(auto i:ans){
     cout<<i<<" "; 
+
  }
+}
+
+vector<int>  prevSmaller(vector<int>&v){
+    //left to right
+    stack<int>s;
+    s.push(-1);
+    vector<int>ans(v.size());
+    for(int i=0;i<v.size();i++){
+      int curr = v[i];
+    while(s.top()>= curr){
+        s.pop();
+    }
+    ans[i] = s.top();
+    s.push(curr);
+    }
+
+return ans;
+}
+
+int main(){
+ vector<int>v;
+ v.push_back(2);
+ v.push_back(3);
+ v.push_back(5);
+ v.push_back(1); 
+ v.push_back(4); 
+// vector<int> ans = nextSmaller(v);
+vector<int> ans1 = prevSmaller(v);
+print(ans1);
 return 0;
  }
